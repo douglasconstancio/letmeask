b9f05ea4c484802d944b5376bb95d12eb698e400
douglasconstancio <douglas.pconstancio@gmail.com>
2021-07-02 22:45:25 -0300

    add creation room screen and add routes management

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,20 +1,13 @@
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 
-export function Home() {
-  const history = useHistory();
-
-  function navigateToNewRoom() {
-    history.push('/rooms/new')
-  }
-
+export function NewRoom() {
   return (
     <div id='page-auth'>
       <aside>
@@ -25,20 +18,19 @@ export function Home() {
       <main>
         <div className='main-content'>
           <img src={logoImg} alt='Letmeask' />
-          <button className='create-room' onClick={navigateToNewRoom}>
-            <img src={googleIconImg} alt='Logo do Google' />
-            Crie sua sala com o Google
-          </button>
-          <div className='separator'>ou entre em uma sala</div>
+          <h2>Criar uma nova sala</h2>
           <form>
             <input
               type='text'
-              placeholder='Digite o código da sala'
+              placeholder='nome da sala'
             />
             <Button type='submit'>
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>
+            Quer entrar em uma sala já existente? <Link to="/"> Clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
